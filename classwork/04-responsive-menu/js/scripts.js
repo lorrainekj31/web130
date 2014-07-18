7038f0e729c7c9fdb6e15a5b61fd80d8c8a54bde
Lorraine Janzen <lorrainekj31@gmail.com>
2014-07-18 00:52:34 +0000

    tried to convert hover to click. Not yet working

--- classwork/04-responsive-menu/js/scripts.js
+++ classwork/04-responsive-menu/js/scripts.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
-  $("toggleMenu").css("display", "none");
-  
-  $(".nav li").hover(function() {
-    $(this).addClass("hover");
-  }, function(){
-    $(this).removeClass("hover");    
+  var ww = document.body.clientwidth;
+  $(".nav li a").each(function() {
+    if(  $(this).next().lenght > 0 ) {
+      $(this).addClass("parent");
+    }
   });
+  
+  if(ww < 800){
+    $(".toggleMenu").css("display", "inline-block");
+    $(".nav li a").click(function(){
+     $(this).parent("li").toggleClass("hover");     
+    });    
+  } else {
+     $(".toggleMenu").css("display", "none"); 
+     $(".nav li").hover(function() {
+      $(this).addClass("hover");
+     }, function(){
+      $(this).removeClass("hover");    
+    });      
+  }   
 });
